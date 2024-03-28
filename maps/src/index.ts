532f1b948f994ef862966c2d023c30d1222ee500
Izabel Moreira <izabelassuncaom@gmail.com>
2024-03-28 15:00:59 -0300

    insert google maps type

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,10 +1,10 @@
-import { User } from './User';
-import { Company } from './Company';
+//import { User } from './User';
+//import { Company } from './Company';
 
-const user = new User();
-console.log(user);
-
-const company = new Company();
-console.log(company);
-
-google;
+const map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
