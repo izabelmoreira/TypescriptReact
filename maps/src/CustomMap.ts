72de9f259d7600b4ad20f35b3afaaa4e84408964
Izabel Moreira <izabelassuncaom@gmail.com>
2024-05-20 17:22:02 -0300

    Showing pop-up window

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,14 +1,12 @@
-import { User } from './User';
-import { Company } from './Company';
-
 //Instructions to every other class
+
 // on how they can be an argument to 'addMarker'
 
 interface Mappable {
   location: {
-    lat: number,
-    lng: number,
-  }
+    lat: number;
+    lng: number;
+  };
 }
 
 export class CustomMap {
@@ -28,22 +26,20 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
     });
-  }
 
-//   addCompanyMarker(company: Company): void {
-//     new google.maps.Marker({
-//       map: this.googleMap,
-//       position: {
-//         lat: company.location.lat,
-//         lng: company.location.lng,
-//       },
-//     });
-//   }
-// }
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'Hi there',
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
